650bbceb32dff524e5c5d819291f484fd95143a4
Zach Cook <zach@groupthreads.com>
2017-08-26 16:19:24 -0500

    initialized project sailsjs project fully.

---
+++ api/controllers/AuthController.js
@@ -0,0 +1,43 @@
+/**
+ * AuthController
+ *
+ * @description :: Server-side logic for managing authentication 
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+ var passport = require('passport');
+
+module.exports = {
+
+	login: function (req, res) {
+		passport.athenticate('local', function (err, user, info) {
+			if ((err) || (!user)) {
+				// If error or user object not passed back
+				return res.send({
+					message: info.message,
+					user: user
+				});
+			}
+
+			// req.login() is a passport method available within request handlers
+			req.logIn(user, function (err) {
+				if (err) {
+					res.send({
+						message: info.message,
+						user: user
+					});
+				}
+			});
+
+		})(req, res); // Posted stackoverflow question about this (req, res) at the end: 
+		// https://stackoverflow.com/questions/45899685/what-does-the-last-parenthetical-on-something-like-functionreq-res-me
+		// TODO: Rewrite in an easier to understand way
+
+	},
+
+	logout: function (req, res) {
+		req.logout();
+		res.redirect();
+	}
+	
+};
---
+++ api/models/User.js
@@ -0,0 +1,30 @@
+/**
+ * User.js
+ *
+ * @description :: Model representing our application's Users
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+ var bcrypt = require('bcrypt');
+
+module.exports = {
+
+  attributes: {
+  	email: { type: 'email', required: true, unique: true },
+  	password: { type: 'string', minLength: 6, required: true },
+  }
+
+  beforeCreate: function (user, cb) {
+  	bcrypt.genSalt(10, function (err, salt) {
+  		bcrypt.hash(user.password, salt, function(err, hash) {
+  			if (err) {
+  				console.log("Error hashing password: ", err);
+  				cb(err);
+  			} else {
+  				user.password = hash;
+  				cb();
+  			}
+  		});
+  	});
+  }
+};
